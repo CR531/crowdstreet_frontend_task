226d1c24a33b55e878b4ee9b119c103e93fda2ba
Reddy531 <chakradhar.lsg4@gmail.com>
2020-11-21 03:12:32 -0600

    Added form layout with data fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+
+    return (
+      <React.Fragment>
+        <div className="div_css">
+          <br />
+          <p className="heading">CrowdStreet Front-End Task</p>
+          <p className="heading">Pre-Approval for an Investor</p>
+          <br />
+          <label className="label">Enter Investment Amount :</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            placeholder="Currency"
+            required
+          /><br />
+          <label className="label">Enter Investment Type :</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            placeholder="Text"
+            required
+          /><br />
+          <label className="label">Enter Total Net Worth :</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            placeholder="Currency"
+            required
+
+          /><br />
+          <label className="label">Enter User Estimated Yearly Income :</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            placeholder="Currency"
+            required
+          /><br />
+          <label className="label">Enter User Estimated Credit Score :</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            placeholder="Number from 300-850"
+            required
+          /><br />
+          <div>
+            <button className="button">Submit</button>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
-
 export default App;
